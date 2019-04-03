970f1612fd85908679b81e7ef1adb13962e45ffc
tylerfrench13 <tylerfrench@u.boisestate.edu>
2019-04-03 00:34:49 -0600

    ngrams used for word by word, sliding window analysis

--- sentiment.py
+++ sentiment.py
@@ -2,6 +2,7 @@
 import numpy as np
 from textblob import TextBlob
 import matplotlib.pyplot as plt
+import time
 
 def moving_average(a, n=3) :
     ret = np.cumsum(a, dtype=float)
@@ -11,37 +12,42 @@ def moving_average(a, n=3) :
 # Fun examples included: raven.txt, hanselgretel.txt, icarus.txt
 # Note: It seems that TextBlob does not like parsing through copy-pasted end quotes.
 #       If you're getting UnicodeDecodeError, that could the problem.
-filename = "transcripts/sc0-inspiration.txt";
+filename = "transcripts/sc0-inspiration.txt"
 transcript = TextBlob(open(filename).read())
 polx = []; poly = []
 subx = []; suby = []
 
-# For every sentence in the selected source file, use TextBlob to evaluate
-# the sentiment (consisting of polarity and subjectivity). Then store those
-# values in two different arrays based on that.
-for sentence in transcript.sentences:
-    poly.append(sentence.sentiment.polarity)
-    polx.append(len(poly))
-    suby.append(sentence.sentiment.subjectivity)
-    subx.append(len(suby))
+# Initialize variables
+numWords = len(transcript.words)                                # Number of words in transcript
+segLength = 10000 if numWords > 100000 else int(numWords/5)     # Segment / window length
+numWindows = numWords - segLength                               # Number of windows to analyze sentiment from
+
+# Print out variables
+print("numWords:", numWords); print("segLength:", segLength); print("numWindows:", numWindows)
+
+# Conduct the sentiment analysis!! We do this according to Reagan's method of gathering all the 
+# words in a sliding window of the text. Each window is analyzed as a whole for sentiment.
+print("Conducting SC Analysis...")
+# Let's keep track of how long this takes; bigger files may become a problem
+startTime = time.time()
+# We use the TextBlob ngrams() function to retrieve all the possible windows of our 
+# specified segLength in the transcript
+for index,window in enumerate(transcript.ngrams(segLength)):
+        poly.append(TextBlob(' '.join(window)).sentiment.polarity)
+        polx.append(len(poly))
+        # Report quarterly progress during analysis
+        if index % int((numWindows / 4)) == 0:
+                print("Finished window", index, "/", numWindows, "[", round((index/numWindows)*100, 2), "% ]")
+
+# Report how long the analysis took
+print("SC Analysis Runtime:", round(time.time() - startTime, 2), "seconds")
 
 # Time to graph! set up the plot with axes and labels
-plt.title(filename);
-plt.ylim(-1, 1)
-plt.plot(moving_average(poly,n=10), linewidth = 1, label = "polarity")
+plt.title(filename)
+#plt.ylim(-1, 1)
+plt.plot(polx, poly, linewidth = 1, label = "polarity")
+#plt.plot(moving_average(poly,n=10), linewidth = 1, label = "polarity")
 #plt.plot(moving_average(suby,n=20), linewidth = 1, label = "subjectivity")
 plt.xlabel("sentence #")
 plt.legend()
 plt.show()
-
-# After you close the plot popup, extreme values of polarity and subjectivity
-# will be printed out, along with their respective sentences.
-print("Extreme Polarity:")
-for index, val in enumerate(poly):
-    if val > 0.9 or val < -0.9:
-        print("[", index, ",", val ,"]", transcript.sentences[index])
-        
-print("\nExtreme Subjectivity:")
-for index, val in enumerate(suby):
-    if val > 0.9 or val < -0.9:
-        print("[", index, ",", val ,"]", transcript.sentences[index])
