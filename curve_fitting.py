2227d8ce76e851a93a57bb445133aec406bbf497
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2019-05-16 13:11:16 -0600

    messed around with curve fitting, but I think I'm missing something. Maybe better to try curve fitting on groups of stories--not sure how to do that.

--- curve_fitting.py
+++ curve_fitting.py
@@ -11,14 +11,25 @@ else:
 
 from scipy.optimize import curve_fit
 
+
 filename = "csv/EthanHollander.csv"
 
 results = np.genfromtxt(filename, delimiter=',', skip_header=1)
 
 author = filename.split("/")[-1].strip(".csv")
+
+
+def sin_func(x,a,b,c):
+    return a * np.sin(b * x) + c
+
+popt, pcov = curve_fit(sin_func, results[:,0], results[:,1])
+
+
 plt.title(author)
-plt.plot(results[:,0],results[:,1], label="Polarity")
-plt.plot(results[:,0],results[:,2], label="Subjectivity")
+plt.plot(results[:,0], results[:,1], label="Polarity")
+#plt.plot(results[:,0],results[:,2], label="Subjectivity")
+plt.plot(results[:,0], sin_func(results[:,0],*popt), 
+        label="fit: a={:.3f}, b={:.3f}, c={:.3f}".format(*popt))
 plt.legend()
 plt.xlabel("Window #")
 plt.show()
