a7d8dfee8b316bb35192c38ca38ae077e168a594
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2019-05-29 11:37:33 -0600

    applies curve fitting to all csvs and stores coefficients to dictionary

--- curve_fitting.py
+++ curve_fitting.py
@@ -1,8 +1,9 @@
 import numpy as np
 import platform
+import glob
 
 # This fixes NSInvalidArgumentException from tkinter on OSX
-if platform.system() == "Darwin": 
+if platform.system() == "Darwin":
     import matplotlib
     matplotlib.use("TkAgg")
     from matplotlib import pyplot as plt
@@ -10,26 +11,43 @@ else:
     import matplotlib.pyplot as plt
 
 from scipy.optimize import curve_fit
+from scipy.signal import savgol_filter
 
 
-filename = "csv/EthanHollander.csv"
-
-results = np.genfromtxt(filename, delimiter=',', skip_header=1)
-
-author = filename.split("/")[-1].strip(".csv")
+def odd(f):
+    """Rounds up to the neareat odd number."""
+    return int(np.ceil(f)//2*2+1)
 
 
-def sin_func(x,a,b,c):
-    return a * np.sin(b * x) + c
+if __name__ == "__main__":
+    deg = 8 # degree of polynomial fit
+    polyfit_dict = {}
 
-popt, pcov = curve_fit(sin_func, results[:,0], results[:,1])
+    for csvfile in glob.glob("csv/*.csv"):
+        filename = csvfile
+        results = np.genfromtxt(filename, delimiter=',', skip_header=1)
+        author = filename.split("/")[-1].strip(".csv")
+
+        # Normalize the polarity data
+        # by shiting by the the mean
+        # and scaling by the std dev
+
+        pol = results[:,1]
+        pol_norm = (pol-np.mean(pol))/np.std(pol)
+
+        # Smooth the data
+
+        smooth_window = odd(len(pol_norm)//10)
+        smooth_pol = savgol_filter(pol_norm, smooth_window, 1)
+
+        # Get percentage of story
+
+        percent = results[:,0]/results[-1,0]*100
+
+        # Fit smoothed, normalized data to polynomial
+        sm_coeffs = np.polyfit(percent,smooth_pol,deg)
+
+        # Save coefficients to dict under author name
+        polyfit_dict[author] = sm_coeffs
 
 
-plt.title(author)
-plt.plot(results[:,0], results[:,1], label="Polarity")
-#plt.plot(results[:,0],results[:,2], label="Subjectivity")
-plt.plot(results[:,0], sin_func(results[:,0],*popt), 
-        label="fit: a={:.3f}, b={:.3f}, c={:.3f}".format(*popt))
-plt.legend()
-plt.xlabel("Window #")
-plt.show()
